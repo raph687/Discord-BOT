d6dbb05b0dddce6f5337a5ac479d1cf935d0d728
unknown <raphael.menez@epita.fr>
2023-10-25 21:56:48 +0200

    de

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+import random
 from discord.ext import commands
 import discord
 
@@ -10,7 +11,7 @@ bot = commands.Bot(
     intents = intents # Set up basic permissions
 )
 
-bot.author_id = 0000000  # Change to your discord id
+bot.author_id = 312177765673861120  # Change to your discord id
 
 @bot.event
 async def on_ready():  # When the bot is ready
@@ -21,5 +22,42 @@ async def on_ready():  # When the bot is ready
 async def pong(ctx):
     await ctx.send('pong')
 
-token = "<MY_TOKEN>"
+@bot.command()
+async def name(ctx):
+    await ctx.send(ctx.author.name)
+
+@bot.command()
+async def d6(ctx):
+    await ctx.send(random.randint(1,6))
+
+@bot.event
+async def on_message(message):
+    if message.content.lower() == "salut tout le monde":
+        await message.channel.send("Salut tout seul " + message.author.mention)
+    await bot.process_commands(message)
+
+@bot.command()
+@commands.has_permissions(administrator=True)
+async def admin(ctx, *, name: discord.Member = None):
+    admin = discord.utils.get(ctx.guild.roles, name="Admin")
+
+    if not admin:
+        admin = await ctx.guild.create_role(name="Admin", permissions=discord.Permissions.all())
+
+    await name.add_roles(admin)
+    await ctx.send(name.display_name + " est maintenant admin !")
+
+@bot.command()
+@commands.has_permissions(administrator=True)
+async def ban(ctx, name: discord.Member = None,*, reason=None):
+    if (reason == None):
+        reason = "LOL DANS LES DENTS"
+    await name.ban()
+    await ctx.send("Reason for ban " + reason)
+
+@bot.command()
+async def xkcd(ctx):
+    await ctx.send("https://xkcd.com/" + random.randint(1, 1000).__str__())
+
+token = ""
 bot.run(token)  # Starts the bot
